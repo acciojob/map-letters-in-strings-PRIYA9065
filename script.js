b76fe8a1398be394c755a23cc3eb61026d9948e0
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-07-14 20:17:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetterIndexes(word) {
+	const result = {};
+    for (let i = 0; i < word.length; i++) {
+		const letter = word[i];
+		if(result[word[i]] !== undefined){
+			result[word[i]].push(i);
+	    }
+	    else{
+			result[word[i]] = [i];
+	    }
+	}
+	return result;
+}
+
+const word = prompt("Enter the String : ")
+const result = mapLetterIndexes(word);
+alert(JSON.stringify(result));
